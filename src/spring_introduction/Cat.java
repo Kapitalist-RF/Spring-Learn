823cf1e0d6bf7c3c3a2667d9d08c9d11efc82bcc
MyName <mozzzart.rf@gmail.com>
2023-06-22 14:57:10 +0300

    Component scan annotations

--- src/spring_introduction/Cat.java
+++ src/spring_introduction/Cat.java
@@ -1,5 +1,9 @@
 package spring_introduction;
 
+import org.springframework.stereotype.Component;
+
+//@Component("catBean")//id для спринг бина!
+@Component//id будет как название класса с мал буквой cat
 public class Cat implements Pet {
     @Override
     public void say() {
