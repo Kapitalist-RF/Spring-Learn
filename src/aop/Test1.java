85159ed952e1dc6990f9a5ae2ebc35de699fbecc
MyName <mozzzart.rf@gmail.com>
2023-06-23 17:15:37 +0300

    Комбинирование Pointcut

--- src/aop/Test1.java
+++ src/aop/Test1.java
@@ -7,11 +7,14 @@ public class Test1 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         UniLibrary uniLibrary = context.getBean("uniLibraryBean", UniLibrary.class);
-        SchoolLibrary schoolLibrary = context.getBean("schoolLibraryBean", SchoolLibrary.class);
+//        SchoolLibrary schoolLibrary = context.getBean("schoolLibraryBean", SchoolLibrary.class);
 //        Book book = context.getBean("book", Book.class);
 
         uniLibrary.getBook();
         uniLibrary.getMagazine();
+        uniLibrary.returnBook();
+        uniLibrary.returnMagazine();
+        uniLibrary.addBook();
 //        uniLibrary.returnBook();
 
         context.close();
--- src/aop/aspect/LoggingAndSecurityAspect.java
+++ src/aop/aspect/LoggingAndSecurityAspect.java
@@ -9,29 +9,78 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAndSecurityAspect {
 
+    @Pointcut("execution(* aop.UniLibrary.*(..))")
+    private void allMethodFromUniLibrary(){}
+
+    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
+    private void returnMagazineFromUniLibrary(){}
+
+    @Pointcut("allMethodFromUniLibrary() && !returnMagazineFromUniLibrary()")
+    private void allMethodExceptReturnMagazineFromUniLibrary(){}
+
+    @Before("allMethodExceptReturnMagazineFromUniLibrary()")
+    public void beforeAllMethodExceptReturnMagazineAdvice(){
+        System.out.println("beforeAllMethodExceptReturnMagazineAdvice: Log #10");
+    }
+
+
+
 //    @Before("execution(public void get*(..))")
 //    public void beforeGetBookAdvice(){
 //        System.out.println("beforeGetBookAdvice: попытка получить книгу");
 //    }
 
-    @Pointcut("execution(* get*())")
-    private void allGetMethod(){
+//    @Pointcut("execution(* get*())")
+//    private void allGetMethod(){
+//
+//    }
+//
+//    @Before("allGetMethod()")
+//    public void beforeGetLoggingAdvice(){
+//        System.out.println("beforeGetLoggingAdvice: попытка получить книгу/журнал");
+//    }
+//
+//    @Before("allGetMethod()")
+//    public void beforeGetSecurityAdvice(){
+//        System.out.println("beforeGetSecurityAdvice: проверка прав на получение книги/журнала");
+//    }
+//
+//    @Before("execution(public * returnBook())")
+//    public void beforeReturnBookAdvice(){
+//        System.out.println("beforeGetBookAdvice: попытка вернуть книгу");
+//    }
 
-    }
+//    @Pointcut("execution(* aop.UniLibrary.get*())")
+//    private void allGetMethodFromUniLibrary(){    }
+//
+//    @Pointcut("execution(* aop.UniLibrary.return*())")
+//    private void allReturnMethodFromUniLibrary(){    }
+//
+//    @Pointcut("allGetMethodFromUniLibrary() || allReturnMethodFromUniLibrary()")
+//    private void allGetAndReturnMethodFromUniLibrary(){}
+//
+//
+//
+//
+//    @Before("allGetMethodFromUniLibrary()")
+//    public void beforeGetLoggingAdvice(){
+//        System.out.println("beforeGetLoggingAdvice: writing Log #1");
+//    }
+//    @Before("allReturnMethodFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice(){
+//        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodFromUniLibrary()")
+//    public void beforeGetAndReturnLoggingAdvice(){
+//        System.out.println("beforeGetAndReturnLoggingAdvice: writing Log #3");
+//    }
 
-    @Before("allGetMethod()")
-    public void beforeGetLoggingAdvice(){
-        System.out.println("beforeGetLoggingAdvice: попытка получить книгу/журнал");
-    }
 
-    @Before("allGetMethod()")
-    public void beforeGetSecurityAdvice(){
-        System.out.println("beforeGetSecurityAdvice: проверка прав на получение книги/журнала");
-    }
 
-    @Before("execution(public * returnBook())")
-    public void beforeReturnBookAdvice(){
-        System.out.println("beforeGetBookAdvice: попытка вернуть книгу");
-    }
+
+
+
+
 
 }
