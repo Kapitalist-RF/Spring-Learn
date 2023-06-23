dd1bf88fa2f5a7d54e05024f0b8604a7c353c6af
MyName <mozzzart.rf@gmail.com>
2023-06-23 20:45:07 +0300

    Advice тип @Around. Знакомство

--- src/aop/aspect/NewLoggingAspect.java
+++ src/aop/aspect/NewLoggingAspect.java
@@ -15,11 +15,14 @@ public class NewLoggingAspect {
     public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("aroundReturnBookLoggingAdvice: в библиотеку пытаются вернуть книгу");
 
-
+        long begin = System.currentTimeMillis();
         Object targetMethodResult = proceedingJoinPoint.proceed();
+        long end = System.currentTimeMillis();
 
 
         System.out.println("aroundReturnBookLoggingAdvice: в библиотеку успешно возвращают книгу");
+        System.out.println("aroundReturnBookLoggingAdvice: метод returnBook выполнил работу за: " +
+                (end - begin) + " миллисекунд");
         return targetMethodResult;
     }
 
