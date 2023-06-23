50746c500ee99c9bd3c5566908abfdd75ad06dce
MyName <mozzzart.rf@gmail.com>
2023-06-23 19:50:58 +0300

    Advice тип @AfterThrowing

--- src/aop/aspect/UniversityLoggingAspect.java
+++ src/aop/aspect/UniversityLoggingAspect.java
@@ -2,6 +2,7 @@ package aop.aspect;
 
 import aop.Student;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,20 +13,29 @@ import java.util.List;
 @Aspect
 public class UniversityLoggingAspect {
 
-    @Before("execution(* getStudents())")
-    public void beforeGetStudentsLoggingAdvice(){
-        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение списка студентов перед методом getStudents");
+//    @Before("execution(* getStudents())")
+//    public void beforeGetStudentsLoggingAdvice(){
+//        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение списка студентов перед методом getStudents");
+//    }
+//
+//    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
+//    public void afterReturningGetStudentsLoggingAdvice(List<Student> students){
+//        Student fst = students.get(0);
+//        String nameS = fst.getNameAndSurname();
+//        nameS = "Mr. " + nameS;
+//        fst.setNameAndSurname(nameS);
+//        fst.setAvgGrade(fst.getAvgGrade() + 5);
+//        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение списка студентов после метода getStudents");
+//    }
+
+    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
+    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception){
+
+        System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс исключения" +
+                exception);
     }
 
-    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
-    public void afterReturningGetStudentsLoggingAdvice(List<Student> students){
-        Student fst = students.get(0);
-        String nameS = fst.getNameAndSurname();
-        nameS = "Mr. " + nameS;
-        fst.setNameAndSurname(nameS);
-        fst.setAvgGrade(fst.getAvgGrade() + 5);
-        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение списка студентов после метода getStudents");
-    }
+
 
 
 
