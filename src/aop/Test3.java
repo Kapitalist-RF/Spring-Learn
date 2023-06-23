b905b6ee59bbed16dcca0b598593955937b0149e
MyName <mozzzart.rf@gmail.com>
2023-06-23 20:39:25 +0300

    Advice тип @Around. Знакомство

---
+++ src/aop/Test3.java
@@ -0,0 +1,23 @@
+package aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        System.out.println("method main start");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        UniLibrary library = context.getBean("uniLibraryBean", UniLibrary.class);
+        String bookName = library.returnBook();
+        System.out.println("В библиотеку вернули книгу: " + bookName);
+
+
+
+        context.close();
+        System.out.println("method main end");
+    }
+
+
+
+
+}
---
+++ src/aop/aspect/NewLoggingAspect.java
@@ -0,0 +1,26 @@
+package aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class NewLoggingAspect {
+
+
+    @Around("execution(String aop.UniLibrary.returnBook())")
+    public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку пытаются вернуть книгу");
+
+
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+
+
+        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку успешно возвращают книгу");
+        return targetMethodResult;
+    }
+
+}
